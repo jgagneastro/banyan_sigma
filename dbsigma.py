a05497f9581b971f8cdb31f9f3b2d56c0dc312b6
jgagneastro <jonathan.gagne.1@gmail.com>
2017-12-09 15:20:02 -0500

    Small additions to readme

--- dbsigma.py
+++ dbsigma.py
@@ -3,6 +3,7 @@ import numpy as np
 from astropy.table import Table
 import pdb
 from scipy.stats import describe #Useful for debugging
+from deg2str import *
 
 #Debugging
 def dbsigma():
@@ -22,8 +23,12 @@ def dbsigma():
 	stars_str = Table.read('/Users/gagne/Downloads/DR7_tot_6_0.fits',format='fits')
 	stars_str = stars_str[np.where((stars_str['PMRA'] != 0) & (stars_str['PMRA'] != -9999) & (stars_str['PMDEC'] != 0) & (stars_str['PMDEC'] != -9999))]
 	
-	column_data = {"EPMRA":"PMRAERR","EPMDEC":"PMDECERR"}
-	test = banyan_sigma(stars_data=stars_str,column_names=column_data)
+	objnames = ['J'+deg2str(rai).replace(':','').split('.')[0]+deg2str(deci,dec=1).replace(':','').split('.')[0] for rai,deci in zip(stars_str['RA'],stars_str['DEC'])]
+	stars_str['OBJNAMES'] = objnames
+	
+	column_data = {"EPMRA":"PMRAERR","EPMDEC":"PMDECERR","NAME":"OBJNAMES"}
+	test = banyan_sigma(stars_data=stars_str,column_names=column_data,use_dist=False,use_rv=False)
+	stop()
 	
 	test = banyan_sigma(ra=stars_str['RA'],dec=stars_str['DEC'],pmra=stars_str['PMRA'],pmdec=stars_str['PMDEC'],epmra=stars_str['PMRAERR'],epmdec=stars_str['PMDECERR'])
 	pdb.set_trace()
